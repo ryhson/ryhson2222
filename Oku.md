8ffd6e3d13e779a4f7e63cf535454fecfed70040
Glitch (hello-express) <none>
2019-12-27 22:56:56 +0000

    🦀💘 Checkpoint ./Oku.md:3068947/413 ./.glitch-assets:3068947/1511 ./hook.js:3068947/7455

--- Oku.md
+++ Oku.md
@@ -1,11 +1,13 @@
-Codare - Youtube Video Bot
 
-.env içerisindeki bölümleri kendinize göre düzenleyiniz.
 
-CALLBACK Website urlnizi yani projenizin urlsini yazınız
-
-HOOKURL Mesajın gönderileceği kanalın webhook urlsini buraya ekleyiniz
-
-PORT 8000 veya 8080 portunu kullanabilirsiniz.
-
-CHID Youtube kanalınız ID'sini giriniz örnek: UCksJCCRy6Phc5W4MSp0Cjrg
+**CODARE - YOUTUBE VİDEO BOT**t
+**
+.en** İiçerisindeki bölümleri kendinize göre düzenleyiniz.
+**
+CALLBAC**  Website urlnizi yani projenizin urlsini yazınız
+**
+HOOKUR**  Mesajın gönderileceği kanalın webhook urlsini buraya ekleyiniz
+**
+POR**  8000 veya 8080 portunu kullanabilirsini.
+**
+CHI**  Youtube kanalınız ID'sini girinizg
--- hook.js
+++ hook.js
@@ -1,104 +1,135 @@
-var parseXml = require('xml2js').parseString;
+var parseXml = require("xml2js").parseString;
 var pubSubHubbub = require("pubsubhubbub");
 var request = require("request").defaults({
-    'headers': {
-        'User-Agent': process.env.UA || 'ytdsc'
-    }
+  headers: {
+    "User-Agent": process.env.UA || "ytdsc"
+  }
 });
 
 if (!process.env.CALLBACK) {
-    console.error("Lütfen CALLBACK urlsi giriniz (Projenizin website urlsini yazmanız gereklidir) nasıl yapılacağını bilmiyorsanız https://discord.gg/p9kFbm4 Discord sucunumuza gelerek yardım alabilirsiniz");
-    process.exit(1);
+  console.error(
+    "Lütfen CALLBACK urlsi giriniz (Projenizin website urlsini yazmanız gereklidir) nasıl yapılacağını bilmiyorsanız https://discord.gg/p9kFbm4 Codare Discord sucunumuza gelerek yardım alabilirsiniz"
+  );
+  process.exit(1);
 }
 
-var channelId = process.env.CHID || 'UCksJCCRy6Phc5W4MSp0Cjrg';
-var topic = 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=' + channelId;
-var hub = 'https://pubsubhubbub.appspot.com/';
+var channelId = process.env.CHID || "UCksJCCRy6Phc5W4MSp0Cjrg";
+var topic =
+  "https://www.youtube.com/xml/feeds/videos.xml?channel_id=" + channelId;
+var hub = "https://pubsubhubbub.appspot.com/";
 
-var lastId = '';
+var lastId = "";
 var isExiting = false;
 
 var pubSubSubscriber = pubSubHubbub.createServer({
-    callbackUrl: process.env.CALLBACK
+  callbackUrl: process.env.CALLBACK
 });
 
-pubSubSubscriber.on('denied', function () {
-    console.error('DENIED', JSON.stringify(arguments));
-    process.exit(2);
+pubSubSubscriber.on("denied", function() {
+  console.error("DENIED", JSON.stringify(arguments));
+  process.exit(2);
 });
 
-pubSubSubscriber.on('error', function () {
-    console.error('ERROR', JSON.stringify(arguments));
-    process.exit(3);
+pubSubSubscriber.on("error", function() {
+  console.error("ERROR", JSON.stringify(arguments));
+  process.exit(3);
 });
 
-setInterval(function () {
-    pubSubSubscriber.subscribe(topic, hub, function (err) { if (err) console.error(err); });
+setInterval(function() {
+  pubSubSubscriber.subscribe(topic, hub, function(err) {
+    if (err) console.error(err);
+  });
 }, 86400000); // refresh subscription every 24 hours
 
-pubSubSubscriber.on('listen', function () {
-    console.log('Kanalınız Gözetleniyor...');
-    // log successful subscriptions
-    pubSubSubscriber.on('subscribe', function (data) {
-        console.log(data.topic + ' subscribed until ' + (new Date(data.lease * 1000)).toLocaleString());
-    });
-    // resubscribe, if unsubscribed while running
-    pubSubSubscriber.on('unsubscribe', function (data) {
-        console.log(data.topic + ' unsubscribed');
-        if (!isExiting) {
-            pubSubSubscriber.subscribe(topic, hub, function (err) { if (err) console.error(err); });
-        }
-    });
-    // Subscribe on start
-    pubSubSubscriber.subscribe(topic, hub, function (err) {
+pubSubSubscriber.on("listen", function() {
+  console.log("Kanalınız Gözetleniyor...");
+  // log successful subscriptions
+  pubSubSubscriber.on("subscribe", function(data) {
+    console.log(
+      data.topic +
+        " subscribed until " +
+        new Date(data.lease * 1000).toLocaleString()
+    );
+  });
+  // resubscribe, if unsubscribed while running
+  pubSubSubscriber.on("unsubscribe", function(data) {
+    console.log(data.topic + " unsubscribed");
+    if (!isExiting) {
+      pubSubSubscriber.subscribe(topic, hub, function(err) {
         if (err) console.error(err);
+      });
+    }
+  });
+  // Subscribe on start
+  pubSubSubscriber.subscribe(topic, hub, function(err) {
+    if (err) console.error(err);
+  });
+  // Parse responses
+  pubSubSubscriber.on("feed", function(data) {
+    var feedstr = data.feed.toString("utf8");
+    parseXml(feedstr, function(err, feed) {
+      if (err) {
+        console.error("ERROR", err);
+      }
+      console.log("JSON:", JSON.stringify(feed.feed));
+      if (feed.feed.entry) {
+        feed.feed.entry.forEach(postToHook);
+      } else console.log("Yeni Video");
     });
-    // Parse responses
-    pubSubSubscriber.on('feed', function (data) {
-        var feedstr = data.feed.toString('utf8');
-        parseXml(feedstr, function (err, feed) {
-            if (err) {
-                console.error("ERROR", err);
-            }
-            console.log("JSON:", JSON.stringify(feed.feed));
-            if (feed.feed.entry) {
-                feed.feed.entry.forEach(postToHook);
-            } else console.log("Yeni Video");
-        });
-    });
+  });
 });
 
 pubSubSubscriber.listen(process.env.PORT || 8000);
 
 function postToHook(entry) {
-    console.log('Son', lastId, 'Şuanki', entry['yt:videoId'][0]);
-    // Ensure it's a video upload and not a duplicate entry
-    if (entry["published"]
-		&& entry["yt:channelId"] == channelId
-		&& lastId != entry['yt:videoId'][0]
-		&& ((new Date(entry['updated'])).getTime() - (new Date(entry['published'])).getTime()) < 60 * 60 * 1000 // 5 min
-	) {
-        lastId = entry['yt:videoId'][0];
-        console.log('newlast', lastId);
-        request.post({
-            url: process.env.HOOKURL,
-            form: {
-                content: "Yeni Video Yüklendi: " + entry["title"] + " - https://youtu.be/" + entry['yt:videoId'][0],
-                embeds: [{
-                    video: "https://youtu.be/" + entry['yt:videoId'][0]
-                }]
+  console.log("Son", lastId, "Şuanki", entry["yt:videoId"][0]);
+  // Ensure it's a video upload and not a duplicate entry
+  if (
+    entry["published"] &&
+    entry["yt:channelId"] == channelId &&
+    lastId != entry["yt:videoId"][0] &&
+    new Date(entry["updated"]).getTime() -
+      new Date(entry["published"]).getTime() <
+      60 * 60 * 1000 // 5 min
+  ) {
+    lastId = entry["yt:videoId"][0];
+    console.log("newlast", lastId);
+    request.post(
+      {
+        url: process.env.HOOKURL,
+        form: {
+          content:
+            "Yeni Video Yüklendi: " +
+            entry["title"] +
+            " - https://youtu.be/" +
+            entry["yt:videoId"][0],
+          embeds: [
+            {
+              video: "https://youtu.be/" + entry["yt:videoId"][0]
             }
-        }, function (err, response, body) {
-            if (err) { console.log('error:', err); }
-            if (response) { console.log('status:', response.statusCode); }
-            if (body) { console.log('body:', body); }
-        });
-    }
+          ]
+        }
+      },
+      function(err, response, body) {
+        if (err) {
+          console.log("error:", err);
+        }
+        if (response) {
+          console.log("status:", response.statusCode);
+        }
+        if (body) {
+          console.log("body:", body);
+        }
+      }
+    );
+  }
 }
 
-
-process.on('SIGINT', function () {
-    isExiting = true;
-    // Unsubscribe on exit
-    pubSubSubscriber.unsubscribe(topic, hub, function (err) { if (err) console.log(err); process.exit(0); });
+process.on("SIGINT", function() {
+  isExiting = true;
+  // Unsubscribe on exit
+  pubSubSubscriber.unsubscribe(topic, hub, function(err) {
+    if (err) console.log(err);
+    process.exit(0);
+  });
 });
